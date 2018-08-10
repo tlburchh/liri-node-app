d740b054891975a3be980c6f2911a36ca5a5c315
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-08-10 07:10:44 -0400

    Spotify is functional. Need to clarify data object so info is more concise

--- liri.js
+++ liri.js
@@ -1,21 +1,34 @@
 require("dotenv").config();
 
-var spotify = new Spotify(MediaKeySession.spotify);
-var client = new Twitter(MediaKeySession.twitter);
-
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify({id:process.env.SPOTIFY_ID, secret:process.env.SPOTIFY_SECRET});
+// var client = new Twitter(keys.twitter);
 
 var option = process.argv[2];
+var option2 = "";
+var nodeArgs = process.argv
 var twitter = "my-tweets";
-var spotify = "spotify-this-song";
+var spotifyThis = "spotify-this-song";
 var movie = "movie-this";
 var doThis = "do-what-i-say";
 
+for (var i =3; i < nodeArgs.length; i++) {
+
+    // Build a string with the address.
+    option2 = option2 + " " + nodeArgs[i];
+  }
 
 if (option === twitter){
     //twitter access code
-}else if (option === spotify){
-    // console.log(spotify);
+}else if (option === spotifyThis){
     //spotify access code
+    spotify.search({ type: 'track', query: option2 }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+              
+      console.log(JSON.stringify(data, null, 2)); 
+      });
 }else if (option === movie){
     //omdb access code
 }else if (option === doThis){
