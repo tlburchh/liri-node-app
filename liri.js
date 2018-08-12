de231c08984d68e971d5389a993856de40440c7c
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-08-12 08:29:49 -0400

    Finishing up calls for omdb ans spotify

--- liri.js
+++ liri.js
@@ -3,15 +3,16 @@ require("dotenv").config();
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify({id:process.env.SPOTIFY_ID, secret:process.env.SPOTIFY_SECRET});
 // var client = new Twitter(keys.twitter);
-// var omdbkey = new Omdb(keys.omdb);
-var omdb = require("omdb");
+var request = require("request");
+var key = require("./keys")
+var fs = require("fs");
 var option = process.argv[2];
 var option2 = "";
-var nodeArgs = process.argv
+var nodeArgs = process.argv;
 var twitter = "my-tweets";
 var spotifyThis = "spotify-this-song";
 var movie = "movie-this";
-var doThis = "do-what-i-say";
+var doThis = "do-what-it-says";
 
 for (var i =3; i < nodeArgs.length; i++) {
 
@@ -29,26 +30,40 @@ if (option === twitter){
         }
         // console.log(data);
         // console.log(data.tracks.items);
-        // var spotData = (JSON.stringify(data, null, 2))
-      console.log("Artist: " + data.tracks.items.artists); 
+        var item = data.tracks.items;
+        console.log(item);
+        for (var i = 0; i < items.artist.length; i++){
+            console.log(item.artist[i]);
+        }
+    //   console.log("Artist: " + data.tracks.items); 
       //********issues navigating to artist object************
       });
 }else if (option === movie){
     //omdb access code
-    omdb.search(option2, function(err, movies) {
-        if(err) {
-            return console.error(err);
+    request("http://www.omdbapi.com/?t="+ option2 + "s&y=&plot=short&apikey="+ key.omdb.apikey , function(error, response, body) {
+
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+    //   console.log(key.omdb.apikey);
+          // Parse the body of the site and recover just the imdbRating
+          // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      
+          var parsedBody = JSON.parse(body);
+          console.log("The movie's rating is: " + parsedBody.imdbRating);
         }
-     
-        if(movies.length < 1) {
-            return console.log('No movies were found!');
-        }
-     
-        movies.forEach(function(movie) {
-            console.log('%s (%d)', movie.title, movie.year);
-        });
-    });
+      });
+      
+
 }else if (option === doThis){
+    console.log(doThis);
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data);
+        var dataArr = data.split(",");
+        console.log(dataArr);
+    });
     //do-what-i-say access code
 }
 
